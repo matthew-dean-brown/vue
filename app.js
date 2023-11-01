746cdc369f809397a1def60e81c808f9980786de
matthew-dean-brown <mattrewbrown@gmail.com>
2023-11-01 21:55:16 +0200

    message

---
+++ app.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+    data: function(){
+        return{
+            title: 'On my way to becoming a Vue Master',
+            name: 'Matthew',
+            verb:'is',
+            sentence:'living his best life at',
+            age:23
+        }
+    }, methods: {
+        
+    },
+})
+
+app.mount('#app');
